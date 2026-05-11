54e19302dde838328fae65c26e4faaa6858b31ea
Pandit upase <panditupase1@gmail.com>
2026-05-11 14:43:25 +0530

    Getting started with angualr,faced error and made changes using copilot

--- src/app/demo-c1/demo-c1.component.spec.ts
+++ src/app/demo-c1/demo-c1.component.spec.ts
@@ -1,3 +1,5 @@
+/// <reference types="jasmine" />
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DemoC1Component } from './demo-c1.component';
@@ -8,7 +10,7 @@ describe('DemoC1Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DemoC1Component]
+      declarations: [DemoC1Component]
     })
     .compileComponents();
 
